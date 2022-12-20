06b837055938b9fc0ac51de1b662d7cec4d2b9f3
Egor Antipichev <antipichevegor@yandex.ru>
2022-12-20 20:20:43 +0300

    Добавил пагинацию к Review и Comments

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, filters, permissions
+from rest_framework.pagination import PageNumberPagination
 from api.serializers import (ReviewSerialiazer, CommentSerializer)
 from reviews.models import (Title, Category, Genre, Review, Comment)
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerialiazer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
 
     def perform_create(self, serializer):
-        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
