a8f8bda5d0ccf30d72e8a71e8624805e9b488c36
vail0 <97312541+vail0@users.noreply.github.com>
2022-12-24 19:47:05 +0300

    registred genres, titles, categories

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,16 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.views import ReviewViewSet, CommentViewSet
+from api.views import (ReviewViewSet, CommentViewSet,
+                       TitleViewSet, GenreViewSet, CategoryViewSet)
 
 app_name = 'api'
 
 router_v1 = routers.DefaultRouter()
-#router_v1.register('titles')
-#router_v1.register('genres')
+router_v1.register('titles', TitleViewSet)
+router_v1.register('genres', GenreViewSet)
+router_v1.register('categories', CategoryViewSet)
+
 router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
                    ReviewViewSet, basename='reviews')
 router_v1.register(
