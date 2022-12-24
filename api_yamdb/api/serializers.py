e4c854213661f70563b6b85eca0d5d46f58909b4
vail0 <97312541+vail0@users.noreply.github.com>
2022-12-24 19:40:33 +0300

    added Title, Category and Genre Serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
+# from rest_framework.validators import UniqueTogetherValidator
 
-from reviews.models import Title, Genre, GenreTitle, Review, Comment
+from reviews.models import Category, Title, Genre, GenreTitle, Review, Comment
 
 
 class ReviewSerialiazer(serializers.ModelSerializer):
@@ -24,3 +25,58 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('author', 'text', 'pub_date')
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Genre
+
+
+# class GenreTitleSerializer(serializers.ModelSerializer):
+#     genre = serializers.SlugRelatedField(
+#         queryset=Genre.objects.all(),
+#         slug_field='genre',
+#         # default=serializers.CurrentUserDefault(),
+#     )
+
+#     title = serializers.SlugRelatedField(
+#         queryset=Title.objects.all(),
+#         slug_field='title',
+#         # default=serializers.CurrentUserDefault(),
+#     )
+
+#     class Meta:
+#         fields = '__all__'
+#         model = GenreTitle
+#         validators = [
+#             UniqueTogetherValidator(
+#                 queryset=GenreTitle.objects.all(),
+#                 fields=('genre', 'title'),
+#             )
+#         ]
+# # 1233
